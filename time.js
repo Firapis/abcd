439254f1e0c98d00f19b7c0de572a9678cd965d9
Firapis <87605838+Firapis@users.noreply.github.com>
2022-01-29 19:29:15 +0900

    Add files via upload

---
+++ time.js
@@ -0,0 +1,11 @@
+<progress id="p1" min="0" max="100" value="0"></progress>
+<button onclick="proc1();">開始</button>
+<script>
+function proc1()
+{
+	if (document.getElementById('p1').value < 100 ) {
+		document.getElementById('p1').value++;
+		setTimeout(proc1, 100);
+	}
+}
+</script>
